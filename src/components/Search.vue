fd9ef8a245c8be4bd36f9a00ac7e1589236a6ca2
Izzy Engelbert <izzyengelbert@gmail.com>
2021-08-10 20:58:30 +0700

    Initial commit.

---
+++ src/components/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row class="">
+    <v-col
+      cols="12"
+      sm="6"
+      md="3"
+      >
+      <v-card
+        color="#385F73"
+        dark
+      >
+        <v-card-text>
+          <v-text-field label="search" v-model="search"></v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+    };
+  },
+  watch: {
+    search: {
+      handler() {
+        this.$emit('change', this.search);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <search @change="onSearch"></search>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+        >
+        <v-row v-for="(item, index) in showedItems" :key="index + item">
+          <v-col>
+            <v-card
+              color="#385F73"
+              dark
+            >
+              <v-card-title class="text-h5">
+                <span>{{ item.name }}</span>
+                <v-spacer></v-spacer>
+                <v-checkbox
+                  v-model="item.selected"
+                ></v-checkbox>
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+        >
+        <v-row v-for="(item, index) in selectedItems" :key="index + item">
+          <v-col>
+            <v-card
+              color="#385F73"
+              dark
+            >
+              <v-card-title class="text-h5">
+                <span>{{ item.name }}</span>
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Search from '@/components/Search.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Search,
+  },
+  data() {
+    return {
+      search: '',
+      listOfItems: [
+        {
+          name: 'Banana',
+          selected: false,
+        },
+        {
+          name: 'Bananana',
+          selected: false,
+        },
+        {
+          name: 'nananana',
+          selected: false,
+        },
+        {
+          name: 'Bana',
+          selected: false,
+        },
+        {
+          name: 'Apple',
+          selected: false,
+        },
+        {
+          name: 'Apple',
+          selected: false,
+        },
+        {
+          name: 'Mikan',
+          selected: false,
+        },
+        {
+          name: 'Nanas',
+          selected: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    onSearch($event) {
+      console.log({ $event });
+      this.search = $event;
+    },
+  },
+  computed: {
+    showedItems() {
+      if (this.search) {
+        return this.listOfItems.filter((item) => item.name.includes(this.search));
+      }
+      return this.listOfItems;
+    },
+    selectedItems() {
+      return this.listOfItems.filter((item) => item.selected);
+    },
   },
 };
 </script>
